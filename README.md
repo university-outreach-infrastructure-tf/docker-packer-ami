44d86fb35a620d3fc0c99743728d711059bc765c
Abhimanyu Narwal <abhimanyunarwal@northwesternmutual.com>
2019-07-30 21:47:39 -0400

    update to use centos base ami

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # docker-packer-ami
 
-Docker CE AMI built using Amazon Linux 2 AMI (AMI-ID:`ami-0b898040803850657`)
+Docker CE AMI built using Centos 7
--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-## Update the installed packages and package cache on your instance
+# Updating your system packages and install the required dependencies
 sudo yum update -y
+sudo yum install yum-utils device-mapper-persistent-data lvm2 -y
 
-## Install the most recent Docker Community Edition package
-sudo amazon-linux-extras install docker
+# Add the Docker stable repository to your system
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
+
+# Install the latest version of Docker CE
+sudo yum install docker-ce -y
 
 # Enable docker on bootup
-sudo systemctl enable docker.service
+sudo systemctl start docker
 
 # Start the Docker service
-sudo  systemctl start docker.service
+sudo systemctl enable docker
 
 # Add the ec2-user to the docker group so you can execute Docker commands without using sudo.
-sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker centos
